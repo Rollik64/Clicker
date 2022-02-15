7fa6c36a9bc064953349726204a4728c7c18211f
Вадим <Rollikak47@gmail.com>
2022-02-15 14:58:36 +0400

    Nikak ne peredam paramentr v HTML

--- src/main/java/com/example/clicker/Controller/ClickController.java
+++ src/main/java/com/example/clicker/Controller/ClickController.java
@@ -2,6 +2,7 @@ package com.example.clicker.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,8 +12,16 @@ public class ClickController {
 
     //Контроллер, который при запросе адреса выдает наш шаблон HTML
     @RequestMapping(value = "/")
-    public String helloWorldController(@RequestParam(name = "name", required = false) String name, Model model) {
-        model.addAttribute("name", name);
+    public String helloWorldController(@RequestParam(name = "counter-label", required = false) String name, Model model) {
+        model.addAttribute("counter-label", name);
         return "helloClick";
     }
+
+    /*
+    @PostMapping(value = "/")
+    public String byeWorldController(@RequestParam(name = "counter-label", required = false) String name, Model model) {
+        Integer.parseInt(name);
+        name++;
+        return name;
+    } */
 }
---
+++ src/main/java/com/example/clicker/DB/TestDB.java
@@ -0,0 +1,13 @@
+package com.example.clicker.DB;
+
+public class TestDB {
+    private int countDB;
+
+    public int getCountDB() {
+        return countDB;
+    }
+
+    public void setCountDB(int countDB) {
+        this.countDB = countDB;
+    }
+}
